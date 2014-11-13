0f378b9182fcd53ad700a0784a304a14432c947d
Taneli Hartikainen <taneli.hartikainen@jamk.fi>
2014-11-13 12:33:05 +0200

    Add ignore rules to 'newrelic' configuration for 400 status codes.

--- newrelic.js
+++ newrelic.js
@@ -3,4 +3,7 @@
 exports.config = {
 	'app_name':    [ 'teamboard-api' ],
 	'license_key': process.env.NEW_RELIC_LICENSE_KEY,
+	'error_collector': {
+		'ignore_status_codes': [ 400, 401, 403, 404 ],
+	}
 }
