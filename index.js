b62c90739126ce62a261b3a9d69791f9f54ed0fd
tanelih <taneli.hartikainen@jamk.fi>
2015-04-13 13:40:33 +0300

    Remove NewRelic. Update JSONWebToken. Remove Cluster support.

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-'use strict';
+'use strict'
 
-var nr      = require('newrelic');
-var os      = require('os');
-var cluster = require('cluster');
-
-// Fork worker processes to each CPU core.
-if(cluster.isMaster) {
-	for(var i = 0; i < os.cpus().length; i++) {
-		cluster.fork();
-	}
-}
-// Run the forked worker process.
-else {
-	var server = require('./server');
-
-	/**
-	 * Attempt some form of cleanup when the process is killed.
-	 */
-	var shutdown = function() {
-		server.shutdown(function() {
-			console.log(
-				'Worker [', cluster.worker.process.pid, '] going down...');
-			process.exit(0);
-		});
-	}
-
-	// Kill the process on 'SIGINT' and 'SIGTERM' signals.
-	process.on('SIGINT', shutdown).on('SIGTERM', shutdown);
-
-	// Start the server application on this worker process.
-	server.listen(function() {
-		console.log('Worker [', cluster.worker.process.pid, '] listening');
-	});
-}
+require('./server').listen();
