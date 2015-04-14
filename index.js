a30850300ec75b4fa818d886d38b559af273fbed
tanelih <taneli.hartikainen@jamk.fi>
2015-04-14 10:03:40 +0300

    Clean up configuration. Testing stuff...

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-console.log('Starting service on port...', process.env.PORT);
-
 require('./server').listen(function() {
 	console.log('Service started on port...', process.env.PORT);
 });
--- utils/emitter.js
+++ utils/emitter.js
@@ -4,8 +4,6 @@ var redis   = require('redis');
 var emitter = require('socket.io-emitter');
 var config  = require('../config');
 
-console.log(config.redis);
-
 var client = redis.createClient(
 	config.redis.port, config.redis.host, config.redis.opts
 );
