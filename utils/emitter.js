ef23e792bc0b2fdd1ed5a96dfde155a23807b86b
tanelih <taneli.hartikainen@jamk.fi>
2015-04-14 14:51:33 +0300

    Add errorhandler to Redis connection.

--- utils/emitter.js
+++ utils/emitter.js
@@ -8,4 +8,12 @@ var client = redis.createClient(
 	config.redis.port, config.redis.host, config.redis.opts
 );
 
+client.on('ready', function() {
+	console.log('Connected to Redis!');
+});
+
+client.on('error', function(err) {
+	console.log(err);
+});
+
 module.exports = emitter(client);
