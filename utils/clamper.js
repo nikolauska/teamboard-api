dc42aa2938f7998fa4e4f23bce5d5e86e8107443
matti <melonmanchan@gmail.com>
2015-05-21 16:39:46 +0300

    Add working clamping

--- utils/clamper.js
+++ utils/clamper.js
@@ -6,7 +6,22 @@ module.exports = function(board) {
 
         return function(ticket) {
             return new Promise(function(resolve, reject) {
-                console.log(ticket.id);
+
+                var tempPosition = {x: ticket.position.x, y: ticket.position.y};
+
+                if(ticket.position.x > (board.size.width * 192) - 96){
+                    tempPosition.x = (board.size.width * 192) - 192;
+                }
+
+                if(ticket.position.y > (board.size.height * 108) - 54){
+                    tempPosition.y = (board.size.height * 108) - 108;
+                }
+
+                ticket.position = tempPosition;
+                ticket.save(function(err) {
+                    // we've saved the dog into the db here
+                    if (err) throw err;
+                });
             });
         }
 }
