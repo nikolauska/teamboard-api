6336a3be067be2bca980c7cec73103c6797fab00
Teemu Kontio <teemu@t-palvelut.net>
2015-06-22 11:11:32 +0300

    Image version query handled via API

--- routes/version.js
+++ routes/version.js
@@ -1,12 +1,25 @@
-'use strict';
 var middleware  = require('../middleware');
 var Router      = require('express').Router();
 
-Router.route('/version')
+const IMG_URL = process.env.IMG_URL || 'http://localhost:9003';
+
+Router.route('/version/api')
     .get(middleware.authenticate('user', 'guest'))
     .get(function(req, res) {
         var version = process.env.VERSION || 'unknown';
         version = '{"version":"'+version+'"}'
         return res.json(200, JSON.parse(version));
     });
+
+Router.route('/version/img')
+    .get(middleware.authenticate('user', 'guest'))
+    .get(function(req, ress) {
+        var version = '{"version":"unknown"}';
+        request.get(IMG_URL+'/version')
+            .end(function(err, res){
+                version = '{"version":"'+res.body.version+'"}';
+                return ress.json(200, JSON.parse(version));
+            });
+    });
+
 module.exports = Router;
