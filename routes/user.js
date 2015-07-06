5e524e2f3fae6e53c513bd439c6ad09a07709738
matti <melonmanchan@gmail.com>
2015-07-06 10:48:25 +0300

    Remove console.log

--- routes/user.js
+++ routes/user.js
@@ -25,7 +25,7 @@ Router.route('/user/edit')
     .put(middleware.authenticate('user'))
     .put(function(req, res, next) {
         var payload = req.body;
-        console.log(payload);
+        
         User.findOne({ '_id': req.user.id }, function(err, user) {
             if(err) {
                 return next(utils.error(500, err));
